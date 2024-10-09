fa912e04f74092e88138f14f5c34a564966b017d
Prashant Rijal <ipatco.inc@gmail.com>
2024-10-09 15:07:23 +0530

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->maxContentWidth('full')
             ->login()
             ->colors([
                 'primary' => '#fe4543'
