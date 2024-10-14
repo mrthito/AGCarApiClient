7f9ec27faf71680dc48125e6968450a624311a65
Prashant Rijal <ipatco.inc@gmail.com>
2024-10-15 00:26:40 +0530

    Update ApplicationLicense.php

--- app/Http/Middleware/ApplicationLicense.php
+++ app/Http/Middleware/ApplicationLicense.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 use Symfony\Component\HttpFoundation\Response;
 
 class ApplicationLicense
@@ -15,6 +17,22 @@ class ApplicationLicense
      */
     public function handle(Request $request, Closure $next): Response
     {
+        $status = Cache::remember('status', 60 * 60, function () {
+            $status = \App\Models\Status::first();
+            $url = Http::get('http://license.ucaryeman.com/index.php?page=verify&lisence=$status->token')->json();
+            if ($url['status'] != 'error') {
+                $status->status = true;
+                $status->save();
+            } else {
+                $status->status = false;
+                $status->save();
+            }
+            return $status;
+        });
+
+        if (!$status || !$status->status) {
+            return response()->json(['status' => 'error', 'message' => 'Application is not licensed.'], 403);
+        }
         return $next($request);
     }
 }
