46824a02b60d6b162c4252a8fcbfd408c9b7a487
Prashant Rijal <ipatco.inc@gmail.com>
2024-10-18 13:12:35 +0530

    er

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,6 @@ Route::middleware([ApplicationLicense::class])->group(function () {
         Route::get('v1/cars/single/{id}', [VehicleController::class, 'single']);
         Route::post('v1/cars/photo/{id}', [VehicleController::class, 'images']);
 
-        Route::get('v1/sliders', [VehicleController::class, 'sliders']);
 
         // change password
         Route::post('v1/change-password', [AuthController::class, 'changePassword']);
@@ -37,6 +36,7 @@ Route::middleware([ApplicationLicense::class])->group(function () {
         Route::get('v1/logout', [AuthController::class, 'logout']);
     });
 
+    Route::get('v1/sliders', [VehicleController::class, 'sliders']);
     // Vehicle
     Route::get('v1/vehicles/car', [VehicleController::class, 'indexNew']);
     Route::get('v1/vehicles/manufacturer', [VehicleController::class, 'manufacturer']);
